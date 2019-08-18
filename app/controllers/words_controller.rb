77976c67686b579132fc55e1863ed32cd83623eb
JUN1113 <dunkelrots@gmail.com>
2019-08-18 12:31:46 +0900

    setting create action

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -3,4 +3,14 @@ class WordsController < ApplicationController
   def new
     @word = @wordbook.words.new
   end
+
+  def create
+    @word = Word.create(word_params)
+    if params[:commit] == "restart"
+      redirect_to new_wordbook_word_path(params[:wordbook_id])
+    else 
+      redirect_to wordbook_path(params[:wordbook_id])
+    end
+  end
+
 end
