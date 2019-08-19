b7411ab0a641fa8f5f66f11c7595d648fdf09694
JUN1113 <dunkelrots@gmail.com>
2019-08-19 17:42:07 +0900

    setting testresult_params

--- app/controllers/testresults_controller.rb
+++ app/controllers/testresults_controller.rb
@@ -1,6 +1,11 @@
 class TestresultsController < ApplicationController
   before_action :authenticate_user!
   before_action :move_to_root, only: :create
+  private
+  def testresult_params
+    params.permit(:test_id).merge(user_id: current_user.id,score: 0)
+  end
+
   def move_to_root
     redirect_to root_path unless params[:testresult]
   end
