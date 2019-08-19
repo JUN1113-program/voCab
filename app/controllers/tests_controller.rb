65fd5805186a71d4770cbe4c75ae8038de92c014
JUN1113 <dunkelrots@gmail.com>
2019-08-19 17:40:57 +0900

    create show action for output test

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,3 +1,8 @@
 class TestsController < ApplicationController
   before_action :authenticate_user!
+  def show
+    @wordbook = Wordbook.find(params[:wordbook_id])
+    @test = @wordbook.test
+    @testresult = @test.testresults.new
+  end
 end
