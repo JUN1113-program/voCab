b4eb48969f7c383339b94e5977d6eb777ffc068b
JUN1113 <dunkelrots@gmail.com>
2019-08-19 17:41:36 +0900

    if user didint input form, user move to root path

--- app/controllers/testresults_controller.rb
+++ app/controllers/testresults_controller.rb
@@ -1,3 +1,7 @@
 class TestresultsController < ApplicationController
   before_action :authenticate_user!
+  before_action :move_to_root, only: :create
+  def move_to_root
+    redirect_to root_path unless params[:testresult]
+  end
 end
