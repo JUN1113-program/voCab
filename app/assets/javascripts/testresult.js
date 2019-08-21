c2991079fe1899fe4c68a53fcbe30ee4c9f5a7fb
JUN1113 <dunkelrots@gmail.com>
2019-08-21 18:48:49 +0900

    chart.js temprate

--- app/assets/javascripts/testresult.js
+++ app/assets/javascripts/testresult.js
@@ -26,26 +26,61 @@ $(function(){
     return colors;
   }
 
+  // グラフを作成する
+  function createChart(ctx = $("#myChart")[0], labels = [1,2,3,4,5], label = "タイトル",data = [1,2,3,4,5], type = "line", axes = 1,labeldisplay = 0,backgroundColor = "rgba(255, 99, 132, 0.7)",borderColor = "rgba(255, 50, 132, 1.0)"){
+    ctx.height = 200;
+    new Chart(ctx, {
+      type: type,
       data: {
-          labels: [1,2,3,4,5],
+          labels: labels,
           datasets: [{
-              label: "テスト結果",
-              data: [Math.random(),Math.random(),Math.random(),Math.random(),Math.random()],
-              backgroundColor: 'rgba(255, 99, 132, 1.0)',
-              borderColor: 'rgba(255, 50, 132, 1.0)',
+              label: label,
+              data: data,
+              backgroundColor: backgroundColor,
+              borderColor: borderColor,
               fill: true
           }]
       },
       options: {
           title:  {
-            display: true,
-            text: "ここがタイトルだよ"
+            display: labeldisplay,
+            text: label
           },
           tooltips: {
             mode: 'point'
+        },
+        scales: {
+          yAxes: [{
+            display: axes,
+            scaleLabel: {
+                display: false,
+                labelString: '得点',
+                fontSize: 18
+            },
+            ticks: {
+                min: 0,
+                max: 110,
+                fontSize: 18,
+                stepSize: 10
+            },
+          }],
+          xAxes: [{
+            display: axes,
+            scaleLabel: {
+                display: false,
+                labelString: '回数',
+                fontSize: 18
+            },
+            ticks: {
+                min: 0,
+                max: labels.length,
+                fontSize: 18,
+                stepSize: 2
+            },
+          }]
         }
       }
-  });
-});
+    });
+  }
 
 
