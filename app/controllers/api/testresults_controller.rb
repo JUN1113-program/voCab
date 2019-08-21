2c98d665c7b9e92d0117fd537c271194f88d3fed
JUN1113 <dunkelrots@gmail.com>
2019-08-21 18:39:11 +0900

    create set_testresult method

--- app/controllers/api/testresults_controller.rb
+++ app/controllers/api/testresults_controller.rb
@@ -1,5 +1,10 @@
 class Api::TestresultsController < ApplicationController
+  before_action :set_testresults
+
   def index
     
+  private
+  def set_testresults
+    @testresults = current_user.testresults
   end
 end
