7f7dd35261b81081e6822327d0cfd74c2edc601a
JUN1113 <dunkelrots@gmail.com>
2019-08-21 18:35:17 +0900

    add new action in testresults api

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show] do
     resources :testresults, only: [:index,:show]
     namespace :api do
-      resources :testresults, only: :index, defaults: { format: 'json' }
+      resources :testresults, only: [:index,:new], defaults: { format: 'json' }
     end
   end
 end
