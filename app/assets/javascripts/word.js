e8cd37b54dc469e02347c9b374a43aeed4d83bba
JUN1113 <dunkelrots@gmail.com>
2019-08-22 14:21:05 +0900

    ajax get wordcards

--- app/assets/javascripts/word.js
+++ app/assets/javascripts/word.js
@@ -1,4 +1,20 @@
 $(function(){
   var showWordBook = $(".main--showWordBook")[0];
   var count = 0;
+
+  // 単語一覧を取得してくる
+  function wordcard (){
+    var wordbook_id = showWordBook.dataset.id;
+    var url = `/wordbooks/${wordbook_id}/api/words`;
+    $.ajax({
+      url: url
+    })
+    .done(function(wordcards){
+      id = wordcards[0].wordbook_id;
+      cards = wordcards;
+    })
+    .fail(function(data){
+      window.alert("error");
+    });
+  }
 });
