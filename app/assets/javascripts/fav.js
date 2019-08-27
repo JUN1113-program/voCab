9d0044c78d486afb61573269bf427c8e12a370c1
JUN1113 <dunkelrots@gmail.com>
2019-08-27 20:45:30 +0900

    非同期通信によるお気に入りの追加

--- app/assets/javascripts/fav.js
+++ app/assets/javascripts/fav.js
@@ -1,2 +1,19 @@
 $(function(){
+  // いいねの生成
+  function like (id, status, button){
+    var wordbook_id = id;
+    var url = `api/wordbooks/${wordbook_id}`;
+    var method = status == "far" ? "PATCH" : "DELETE";
+    $.ajax({
+      url: url,
+      type: method
+    })
+    .done(function(){
+      button.toggleClass("far fa-3x fa-bookmark");
+      button.toggleClass("fas fa-3x fa-bookmark");
+    })
+    .fail(function(){
+      window.alert("error");
+    });
+  }
 });
