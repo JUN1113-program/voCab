ca2d5bea346f4af3426896abc565e6a7a3dc50cd
JUN1113 <dunkelrots@gmail.com>
2019-08-27 18:59:32 +0900

    destroy action for favorite

--- app/controllers/api/wordbooks_controller.rb
+++ app/controllers/api/wordbooks_controller.rb
@@ -2,4 +2,8 @@ class Api::WordbooksController < ApplicationController
   def update
     @fav = Favorite.create(user_id: current_user.id,wordbook_id: params[:id])
   end
+  def destroy
+    @fav = Favorite.where(wordbook_id: params[:id]).find_by(user_id: current_user.id)
+    @fav.destroy
+  end
 end
