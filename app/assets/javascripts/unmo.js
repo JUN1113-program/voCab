713c0ecfde0f98209eb60339ba451d53a86204a7
JUN1113 <dunkelrots@gmail.com>
2019-09-14 23:46:40 +0900

    入力を用いてunmoの非同期通信を行う

--- app/assets/javascripts/unmo.js
+++ app/assets/javascripts/unmo.js
@@ -2,4 +2,21 @@ $(function(){
   var responseFiled = $(".unmo__resp");
   var input = $(".unmo__form__input");
   var icon = $(".unmo__icon");
+  function unmo (input){
+    data = {input: input}
+    var url = "/api/unmos"
+    $.ajax({
+      url: url,
+      data: data
+    })
+    .done(function(data){
+      responseFiled.html(data.response);
+      $(".unmo__form__input").val("");
+      changeImage(randomNum());
+    })
+    .fail(function(){
+      window.alert("error");
+    });
+  }
+
 });
