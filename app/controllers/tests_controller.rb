8d6c1f2f533fa1270ff695e18d124ff0b1d9cd1e
JUN1113 <dunkelrots@gmail.com>
2019-10-02 15:37:27 +0900

    test create action

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -4,6 +4,19 @@ class TestsController < ApplicationController
     @test = Test.new
   end
 
+  def create
+    @test = Test.create(test_params)
+    @wordbook = @test.wordbook
+    testword_params.each.with_index do |words, i|
+      @question = Question.create(test_id: @test.id, word_id: @wordbook.words[i].id)
+      words.each.with_index do |word, j|
+        correct = j == 0 ? 1 : 0
+        Testword.create(word: word, correct: correct, question_id: @question.id)
+      end
+    end
+    redirect_to wordbook_words_path(@wordbook.id)
+  end
+
   def show
     @wordbook = Wordbook.find(params[:wordbook_id])
     @test = @wordbook.test
