9644364dee2001041f473ba226c44e8a49b34306
JUN1113 <dunkelrots@gmail.com>
2019-10-02 15:37:13 +0900

    test new action

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,9 @@
 class TestsController < ApplicationController
   before_action :authenticate_user!
+  def new
+    @test = Test.new
+  end
+
   def show
     @wordbook = Wordbook.find(params[:wordbook_id])
     @test = @wordbook.test
