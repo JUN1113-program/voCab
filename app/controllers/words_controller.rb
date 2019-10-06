548516353d72ee190715bb036ef510d4e2e15e07
JUN1113 <dunkelrots@gmail.com>
2019-10-06 20:43:18 +0900

    単語カード作成画面の一覧表示用にwordsを定義

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,10 +2,12 @@ class WordsController < ApplicationController
   before_action :authenticate_user!, except: :index
   before_action :set_wordbook
   before_action :set_word, only: [:edit,:update,:destroy]
+  before_action :set_words, only: [:new, :edit]
   before_action :his_wordbook?, except: :index
 
   def index
   end
+
   def new
     @word = @wordbook.words.new
   end
@@ -33,12 +35,19 @@ class WordsController < ApplicationController
   def word_params
     params.require(:word).permit(:front,:reverse).merge(wordbook_id: params[:wordbook_id])
   end
+
   def set_wordbook
     @wordbook = Wordbook.find(params[:wordbook_id])
   end
+
   def set_word
     @word = @wordbook.words.find(params[:id])
   end
+
+  def set_words
+    @words = @wordbook.words.order("updated_at DESC")
+  end
+
   def his_wordbook?
     redirect_to root_path unless current_user.id == @wordbook.user.id
   end
