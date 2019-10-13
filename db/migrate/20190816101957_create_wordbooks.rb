ef1f2b6ad47c1721346b094c33de402cba263235
JUN1113 <dunkelrots@gmail.com>
2019-10-13 21:06:05 +0900

    wordbook用マイグレーションを一つにまとめる

--- db/migrate/20190816101957_create_wordbooks.rb
+++ db/migrate/20190816101957_create_wordbooks.rb
@@ -3,7 +3,9 @@ class CreateWordbooks < ActiveRecord::Migration[5.2]
     create_table :wordbooks do |t|
       t.string :title, null: false
       t.integer :viewed, null: false
+      t.boolean :share, null: false
       t.string :reference
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
