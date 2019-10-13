1dabf256dfd6f2790f0ed8dcb13f51ce82a0ea1f
JUN1113 <dunkelrots@gmail.com>
2019-10-14 01:55:44 +0900

    adminのindexアクション

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,9 @@
 class AdminsController < ApplicationController
   before_action :admin?
+
+  def index
+    @users = User.includes(:wordbooks)
+  end
   private
   def admin?
     redirect_to root_path unless current_user&.admin
