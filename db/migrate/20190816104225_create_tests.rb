3f695f42b82e61d95f9d2444e3c4affcec607192
JUN1113 <dunkelrots@gmail.com>
2019-10-13 21:06:22 +0900

    テストを受験した回数を削除

--- db/migrate/20190816104225_create_tests.rb
+++ db/migrate/20190816104225_create_tests.rb
@@ -1,7 +1,6 @@
 class CreateTests < ActiveRecord::Migration[5.2]
   def change
     create_table :tests do |t|
-      t.integer :times, null: false
       t.references :wordbook, foreign_key: true
       t.timestamps
     end
