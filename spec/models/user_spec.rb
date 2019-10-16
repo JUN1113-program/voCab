48c3868f6dc59a9a5d8580d51b1dc6b5b2d4319a
JUN1113 <dunkelrots@gmail.com>
2019-10-16 17:12:15 +0900

    登録できない場合のテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,5 +13,43 @@ describe User do
     end
   end
 
+  context "登録できない場合" do
+    it "nicknameが未入力である" do
+      user = build(:user, nickname: "")
+      user.valid?
+      expect(user.errors[:nickname]).to include("を入力してください")
+    end
+
+    it "emailが未入力である" do
+      user = build(:user, email: "")
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
+
+    it "重複したemailが入力された" do
+      user = create(:user)
+      another_user = build(:user)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("はすでに存在します")
+    end
+
+    it "passwordが未入力である" do
+      user = build(:user, password: "")
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
+
+    it "passwordとpassword_confirmationが不一致である" do
+      user = build(:user, password: "123456", password_confirmation: "654321")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("とPasswordの入力が一致しません")
+    end
+
+    it "パスワードが5文字入力されている" do
+      user = build(:user, password: "12345", password_confirmation: "12345")
+      user.valid?
+      expect(user.errors[:password]).to include("は6文字以上で入力してください")
+    end
+
   end
 end
