abce0f6200e3ae061c5c3c27dcb7d3322b5e0865
JUN1113 <dunkelrots@gmail.com>
2019-10-16 17:12:06 +0900

    登録できる場合のテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 describe User do
-  describe '' do
-    it "" do
+
+  context "登録できる場合" do
+    it "nickname, emailが入力されており、password, password_confirmationが一致している" do
+      user = build(:user)
+      expect(user).to be_valid
     end
+
+    it "パスワードが6文字入力されている" do
+      user = build(:user, password: "123456", password_confirmation: "123456")
+      expect(user).to be_valid
+    end
+  end
+
   end
 end
