58ce910380b671e9a5d16bcbdfbb0cafbd1eb913
JUN1113 <dunkelrots@gmail.com>
2019-10-16 17:04:22 +0900

    factorybotを定義

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
 
   factory :user do
+    nickname              {"test"}
+    email                 {"test@test"}
+    password              {"000000"}
+    password_confirmation {"000000"}
   end
 
 end
