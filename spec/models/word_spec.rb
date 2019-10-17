aa55762a0f4d6c3535ae9bc9b9b8d22456610216
JUN1113 <dunkelrots@gmail.com>
2019-10-17 16:10:50 +0900

    登録できる場合

--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -1,4 +1,22 @@
 require 'rails_helper'
 describe Word do
 
+  context "登録できる場合" do
+
+    it "すべての値が適切に入力されている" do
+      word = build(:word)
+      expect(word).to be_valid
+    end
+
+    it "frontが30文字" do
+      word = build(:word, front: Faker::Alphanumeric.alpha(number: 30))
+      expect(word).to be_valid
+    end
+
+    it "reverseが30文字" do
+      word = build(:word, reverse: Faker::Alphanumeric.alpha(number: 30))
+      expect(word).to be_valid
+    end
+
+  end
 end
