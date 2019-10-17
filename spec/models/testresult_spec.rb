7a85877eea429811d57ad9f7f2c2eae1f70e6c6b
JUN1113 <dunkelrots@gmail.com>
2019-10-17 16:55:30 +0900

    testresultを登録できない場合

--- spec/models/testresult_spec.rb
+++ spec/models/testresult_spec.rb
@@ -10,4 +10,42 @@ describe Testresult do
 
   end
 
+  context "登録できない場合" do
+
+    it "scoreが入力されていない" do
+      testresult = build(:testresult, score: "")
+      testresult.valid?
+      expect(testresult.errors[:score]).to include("を入力してください")
+    end
+
+    it "scoreが文字列" do
+      testresult = build(:testresult, score: "aaa")
+      testresult.valid?
+      expect(testresult.errors[:score]).to include("は数値で入力してください")
+    end
+
+    it "scoreが0を下回る" do
+      testresult = build(:testresult, score: -1)
+      testresult.valid?
+      expect(testresult.errors[:score]).to include("は0以上の値にしてください")
+    end
+
+    it "scoreが100を上回る" do
+      testresult = build(:testresult, score: 101)
+      testresult.valid?
+      expect(testresult.errors[:score]).to include("は100以下の値にしてください")
+    end
+
+    it "user_idがない" do
+      testresult = build(:testresult, user_id: "")
+      testresult.valid?
+      expect(testresult.errors[:user]).to include("を入力してください")
+    end
+
+    it "test_idがない" do
+      testresult = build(:testresult, test_id: "")
+      testresult.valid?
+      expect(testresult.errors[:test]).to include("を入力してください")
+    end
+  end
 end
