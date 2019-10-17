61b51e3729525e04b9a86cc8f3854c2ec446f2fa
JUN1113 <dunkelrots@gmail.com>
2019-10-17 16:53:47 +0900

    questionの登録できる場合

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,3 +1,13 @@
 require 'rails_helper'
 describe Question do
+
+  context "登録できる場合" do
+
+    it "すべての値が適切に入力されている" do
+      question = build(:question)
+      expect(question).to be_valid
+    end
+
+  end
+
 end
