2d75246ab558ff4a1e87cffab7ce794808dc6ec7
JUN1113 <dunkelrots@gmail.com>
2019-10-17 16:55:20 +0900

    testresultを登録できる場合

--- spec/models/testresult_spec.rb
+++ spec/models/testresult_spec.rb
@@ -1,3 +1,13 @@
 require 'rails_helper'
 describe Testresult do
+
+  context "登録できる場合" do
+
+    it "すべての値が適切に入力されている" do
+      testresult = build(:testresult)
+      expect(testresult).to be_valid
+    end
+
+  end
+
 end
