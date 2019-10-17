4b979bb30a8f97ff7ec3919318e5441550914a5c
JUN1113 <dunkelrots@gmail.com>
2019-10-17 16:54:07 +0900

    questionの登録できない場合

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -10,4 +10,20 @@ describe Question do
 
   end
 
+  context "登録できない場合" do
+
+    it "word_idがない" do
+      question = build(:question, word_id: "")
+      question.valid?
+      expect(question.errors[:word]).to include("を入力してください")
+    end
+
+    it "test_idがない" do
+      question = build(:question, test_id: "")
+      question.valid?
+      expect(question.errors[:test]).to include("を入力してください")
+    end
+
+  end
+
 end
