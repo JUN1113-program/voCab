0a02c0db119fdc12b67ebae407c3f00fca12b1fb
JUN1113 <dunkelrots@gmail.com>
2019-10-17 16:56:30 +0900

    testwordを登録できる場合

--- spec/models/testword_spec.rb
+++ spec/models/testword_spec.rb
@@ -1,3 +1,13 @@
 require 'rails_helper'
 describe Testword do
+
+  context "登録できる場合" do
+
+    it "すべての値が適切に入力されている" do
+      testword = build(:testword)
+      expect(testword).to be_valid
+    end
+
+  end
+
 end
