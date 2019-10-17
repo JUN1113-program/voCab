e8be4da12994319639a38155ca0cb08a051296c6
JUN1113 <dunkelrots@gmail.com>
2019-10-17 16:10:58 +0900

    登録できない場合

--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -19,4 +19,39 @@ describe Word do
     end
 
   end
+
+  context "登録できない場合" do
+
+    it "frontが未入力である" do
+      word = build(:word, front: "")
+      word.valid?
+      expect(word.errors[:front]).to include("を入力してください")
+    end
+
+    it "frontが30文字を超える" do
+      word = build(:word, front: Faker::Alphanumeric.alpha(number: 31))
+      word.valid?
+      expect(word.errors[:front]).to include("は30文字以内で入力してください")
+    end
+
+    it "reverseが30文字を超える" do
+      word = build(:word, reverse: Faker::Alphanumeric.alpha(number: 31))
+      word.valid?
+      expect(word.errors[:reverse]).to include("は30文字以内で入力してください")
+    end
+
+    it "reverseが未入力" do
+      word = build(:word, reverse: "")
+      word.valid?
+      expect(word.errors[:reverse]).to include("を入力してください")
+    end
+
+    it "wordbook_idがない" do
+      word = build(:word, wordbook_id: nil)
+      word.valid?
+      expect(word.errors[:wordbook]).to include("を入力してください")
+    end
+
+  end
+
 end
